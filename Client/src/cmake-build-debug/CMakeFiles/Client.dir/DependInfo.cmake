80e4e85e1e2815793281a996f9970c8b3f26e7e7
galm <gal.markovich@gmail.com>
2022-08-20 22:21:20 +0300

    small changes

---
+++ Client/src/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/galmarko/git/Social-Media/Client/src/BidiMessagingProtocol.cpp" "CMakeFiles/Client.dir/BidiMessagingProtocol.cpp.o" "gcc" "CMakeFiles/Client.dir/BidiMessagingProtocol.cpp.o.d"
+  "/home/galmarko/git/Social-Media/Client/src/KeyboardReader.cpp" "CMakeFiles/Client.dir/KeyboardReader.cpp.o" "gcc" "CMakeFiles/Client.dir/KeyboardReader.cpp.o.d"
+  "/home/galmarko/git/Social-Media/Client/src/SocketReader.cpp" "CMakeFiles/Client.dir/SocketReader.cpp.o" "gcc" "CMakeFiles/Client.dir/SocketReader.cpp.o.d"
+  "/home/galmarko/git/Social-Media/Client/src/connectionHandler.cpp" "CMakeFiles/Client.dir/connectionHandler.cpp.o" "gcc" "CMakeFiles/Client.dir/connectionHandler.cpp.o.d"
+  "/home/galmarko/git/Social-Media/Client/src/echoClient.cpp" "CMakeFiles/Client.dir/echoClient.cpp.o" "gcc" "CMakeFiles/Client.dir/echoClient.cpp.o.d"
+  "/home/galmarko/git/Social-Media/Client/src/encoderDecoder.cpp" "CMakeFiles/Client.dir/encoderDecoder.cpp.o" "gcc" "CMakeFiles/Client.dir/encoderDecoder.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Client/src/cmake-build-debug/CMakeFiles/Client.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Client.dir/BidiMessagingProtocol.cpp.o"
+  "CMakeFiles/Client.dir/BidiMessagingProtocol.cpp.o.d"
+  "CMakeFiles/Client.dir/KeyboardReader.cpp.o"
+  "CMakeFiles/Client.dir/KeyboardReader.cpp.o.d"
+  "CMakeFiles/Client.dir/SocketReader.cpp.o"
+  "CMakeFiles/Client.dir/SocketReader.cpp.o.d"
+  "CMakeFiles/Client.dir/connectionHandler.cpp.o"
+  "CMakeFiles/Client.dir/connectionHandler.cpp.o.d"
+  "CMakeFiles/Client.dir/echoClient.cpp.o"
+  "CMakeFiles/Client.dir/echoClient.cpp.o.d"
+  "CMakeFiles/Client.dir/encoderDecoder.cpp.o"
+  "CMakeFiles/Client.dir/encoderDecoder.cpp.o.d"
+  "Client"
+  "Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
